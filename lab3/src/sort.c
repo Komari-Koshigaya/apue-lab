0bbde2f8925af902f6df3fcd12d23053c8632241
niejun <2323384399@qq.com>
2020-03-29 11:01:03 +0800

    init sorting struct[] in c

---
+++ lab3/src/sort.c
@@ -0,0 +1,12 @@
+#include <head.h>
+
+/**
+ * 根据结构体的某个字段进行排序,此处未当前目录文件结构体
+ * desc是排序方式 1 顺序 0 逆序，最后一个是 排序的字段
+ * 默认按文件名从a-z排序
+ */
+void sort(DIR *currentdir,int desc,char *argu)
+{
+	// struct dirent *currentdp[];
+	// while( (currentdp = readdir(currentdir)) != NULL )
+}
